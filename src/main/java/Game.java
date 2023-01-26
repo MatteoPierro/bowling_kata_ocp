6367dc094158403635560da30c381eba8c640b41
Matteo Pierro <pierromatteo@gmail.com>
2023-01-26 23:51:03 +0200

    first frame with no pins knocked down

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,4 +2,8 @@ public class Game {
     public int score() {
         return 0;
     }
+
+    public void roll(int knockedDownPins) {
+
+    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -9,4 +9,14 @@ public class GameTest {
         Game game = new Game();
         assertThat(game.score()).isEqualTo(0);
     }
+
+    @Test
+    void when_0_pins_are_knocked_down_in_the_first_frame_the_score_is_0() {
+        Game game = new Game();
+
+        game.roll(0);
+        game.roll(0);
+
+        assertThat(game.score()).isEqualTo(0);
+    }
 }
