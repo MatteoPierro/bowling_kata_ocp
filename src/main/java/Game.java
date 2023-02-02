65cd533f8f2dab47506f056d7c878dc394d260e5
Joe Bew <joebew42@gmail.com>
2023-02-02 22:54:00 +0100

    refactor: extract method

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -8,13 +8,18 @@ public class Game {
     public int score() {
         int score = 0;
         for (int rollIndex = 0; rollIndex < rolls.size();) {
-            score += rolls.get(rollIndex);
-            if (rollIndex + 1 < rolls.size()) {
-                score += rolls.get(rollIndex + 1);
-            }
+            score += frameScore(rollIndex);
             rollIndex += 2;
         }
         return score;
+    }
+
+    private Integer frameScore(int rollIndex) {
+        Integer frameScore = rolls.get(rollIndex);
+        if (rollIndex + 1 < rolls.size()) {
+            frameScore += rolls.get(rollIndex + 1);
+        }
+        return frameScore;
     }
 
     public void roll(int knockedDownPins) {
